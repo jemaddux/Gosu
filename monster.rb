fa8dcf5b124f3a143c509e83b266c24cc3c795f1
John Maddux <jemaddux@gmail.com>
2016-03-12 19:02:56 -0500

    Monster 3

--- monster.rb
+++ monster.rb
@@ -9,17 +9,31 @@ class Monster
     @mana = mana
   end
 
+  def list_attribute(attribute, attribute_name)
+    if attribute_name.end_with?("s")
+      verb = "are"
+    else
+      verb = "is"
+    end
+    puts "#{name}'s #{attribute_name} #{verb} #{attribute}"
+  end
+
   def list_attributes
     puts name
-    puts "#{name}'s height is #{height}"
-    puts "#{name}'s strength is #{strength}"
-    puts "#{name}'s health points are #{health_points}"
-    puts "#{name}'s mana is #{mana}"
+    # puts "#{name}'s height is #{height}"
+    list_attribute(height, "height")
+    # puts "#{name}'s strength is #{strength}"
+    list_attribute(strength, "strength")
+    # puts "#{name}'s health points are #{health_points}"
+    list_attribute(health_points, "health points")
+    # puts "#{name}'s mana is #{mana}"
+    list_attribute(mana, "mana")
   end
 
   def attack(target)
 
   end
+
 end
 
 zoidberg = Monster.new("Zoidberg", 50, 4, 80, 0)
